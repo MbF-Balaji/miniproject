055660a7803cd49b68e8dda750bf3640ed14a6e6
balambf <balaji.d@madebyfire.com>
2017-05-30 11:32:54 +0530

    session issue solved and html pages modified

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,30 +2,33 @@ class UserController < ApplicationController
   
 
   def new 
-    @form1 = params[:name]
-    session[:value] = @form1
+    session[:value] = params[:name]
     if session[:value].present?
-     redirect_to user_step1_path
+     redirect_to user_step2_path
     end
    end
-
+def step1
+    session[:value3] = params[:city]
+  end
   def save 
-     @verification = UserDetail.new
+     @verification = UserDetail.new()
+     @verification.name = session[:value]
+     @verification.city = session[:value2]
      if @verification.save
+      flash[:success] = "your data saved successfully"
       redirect_to user_new_path
     end 
   end
-  def step1
-    @form1 = session[:value]
-    @form2 = @form1
-  end
+  
 
   def step2
-    
+    session[:value2] = params[:email]
   # # @lang_known = Languages_Known.new
   # session[:value] = params[:user_language]
   #    @form2 = session[:value]
-     
+     if session[:value2].present?
+     redirect_to user_step1_path
+    end
   end
  #  def create
  #    @verification = UserDetail.new(params[:country])
