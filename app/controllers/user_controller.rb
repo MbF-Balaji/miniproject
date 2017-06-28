486f13023884bdd3be13682ae8921b1db6bcc5c5
balambf <balaji.d@madebyfire.com>
2017-06-28 18:21:57 +0530

    Newly added files based on session issue solved

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,37 +2,83 @@ class UserController < ApplicationController
   
 
   def new 
-    session[:value] = params[:name]
-      if session[:value].present?
-     redirect_to user_step1_path
+    session[:value0] = params[:name]
+    session[:value1] = params[:email]
+    session[:value2] = params[:phone]
+    session[:value3] = params[:city]
+    session[:value4] = params[:country]
+    if session[:value0].present?
+    redirect_to user_step1_path
+    end
+  end
+  def new_data   
+    # respond_to do |format|
+    #   if session[:value0] == "test"
+    #     format.html { redirect_to user_step2_path }
+    #   else
+    #     format.html { redirect_to user_step2_path }
+    #   end
+    # end
+  end
+  def extra
+    session[:value0] = params[:name]
+    session[:value1] = params[:email]
+    session[:value2] = params[:phone]
+    session[:value3] = params[:city]
+    session[:value4] = params[:country]
+    if session[:value0].present?
+    redirect_to user_step2_path
     end
   end
   def step1
-    session[:value2] = params[:skills]
-    if session[:value2].present?
-     redirect_to user_step2_path
+    @lang = LanguagesCollection.all
+    session[:value5] = params[:school_name]
+    session[:value6] = params[:percentage]
+    session[:value7] = params[:institution_name]
+    session[:value8] = params[:percentage2]
+    session[:value9] = params[:skills]
+    session[:value10] = params[:languages]
+     if session[:value6].present?
+    redirect_to user_step2_path
     end
   end
+   def step1_data
+    @lang = LanguagesCollection.all
+  end
   def save 
-     @verification = UserDetail.new()
-     @verification.name = session[:value]
-     @verification.city = session[:value2]
-     if @verification.save
+     @user = UserDetail.new()
+     @quali = Qualification.new()
+     @addskills = AdditionalSkill.new()
+     @language = LanguagesKnown.new()
+     @user.name = session[:value0]
+     @user.email = session[:value1]
+     @user.phone = session[:value2]
+     @user.city = session[:value3]
+     @user.country = session[:value4]
+     @quali.school_institution_name = session[:value5]
+     @quali.percentage = session[:value6]
+     @quali.institution = session[:value7]
+     @quali.insti_perc = session[:value8]
+     @addskills.skills = session[:value9]
+     @language.user_language = session[:value10]
+     if @user.save && @quali.save
       flash[:success] = "your data saved successfully"
       redirect_to user_new_path
     end 
+    
   end
   
 
   def step2
-    session[:value3] = params[:email]
-  # # @lang_known = Languages_Known.new
-  # session[:value] = params[:user_language]
-  #    @form2 = session[:value]
-    if session[:value3].present?
-     redirect_to user_step1_path
-    end
+  #   session[:value3] = params[:email]
+  # # # @lang_known = Languages_Known.new
+  # # session[:value] = params[:user_language]
+  # #    @form2 = session[:value]
+  #   if session[:value3].present?
+  #    redirect_to user_step1_path
+  #   end
   end
+ 
  #  def create
  #    @verification = UserDetail.new(params[:country])
  # #     @post = params[:name]
@@ -43,7 +89,7 @@ class UserController < ApplicationController
  #       render :step2
  #       end
  #  end
-    def test_params
-       params.permit(:city)
+    def user_params
+      params.require(:userdetail).permit(:name, :email, :phone, :city, :country)
      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   get 'user/new' , :to => "user#new"
   post 'user/new' , :to => "user#new"
+  post 'user/extra' , :to => "user#extra"
+  get 'user/new_data' , :to => "user#new_data"
+  post 'user/new_data' , :to => "user#step2"
   get 'user/step1', :to => "user#step1"
   post 'user/step1', :to => "user#step1"
+  get 'user/step1_data', :to => "user#step1_data"
+  post 'user/step1_data', :to => "user#step2"
   get 'user/step2', :to => "user#step2"
   post 'user/step2', :to => "user#save"
+  
   root to:'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
