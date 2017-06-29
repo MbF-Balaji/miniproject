f9f8728eae0ff50219bc2d0731717f28b375fe80
balambf <balaji.d@madebyfire.com>
2017-06-29 20:18:05 +0530

    Session issue has been solved in a  clean way

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,48 +2,26 @@ class UserController < ApplicationController
   
 
   def new 
-    session[:value0] = params[:name]
-    session[:value1] = params[:email]
-    session[:value2] = params[:phone]
-    session[:value3] = params[:city]
-    session[:value4] = params[:country]
-    if session[:value0].present?
+    if request.method == 'POST'
+    session[:value0] = params[:name]     if params[:name].present?
+    session[:value1] = params[:email]    if params[:email].present?
+    session[:value2] = params[:phone]    if params[:phone].present?
+    session[:value3] = params[:city]     if params[:city].present?
+    session[:value4] = params[:country]  if params[:country].present?
     redirect_to user_step1_path
-    end
   end
-  def new_data   
-    # respond_to do |format|
-    #   if session[:value0] == "test"
-    #     format.html { redirect_to user_step2_path }
-    #   else
-    #     format.html { redirect_to user_step2_path }
-    #   end
-    # end
-  end
-  def extra
-    session[:value0] = params[:name]
-    session[:value1] = params[:email]
-    session[:value2] = params[:phone]
-    session[:value3] = params[:city]
-    session[:value4] = params[:country]
-    if session[:value0].present?
-    redirect_to user_step2_path
-    end
-  end
+end
   def step1
     @lang = LanguagesCollection.all
-    session[:value5] = params[:school_name]
-    session[:value6] = params[:percentage]
-    session[:value7] = params[:institution_name]
-    session[:value8] = params[:percentage2]
-    session[:value9] = params[:skills]
-    session[:value10] = params[:languages]
-     if session[:value6].present?
+    if request.method == 'POST'
+    session[:value5] = params[:school_name]        if params[:school_name].present?
+    session[:value6] = params[:percentage]         if params[:percentage].present?
+    session[:value7] = params[:institution_name]   if params[:institution_name].present?
+    session[:value8] = params[:percentage2]        if params[:percentage2].present?
+    session[:value9] = params[:skills]             if params[:skills].present?
+    session[:value10] = params[:languages]         if params[:languages].present?
     redirect_to user_step2_path
     end
-  end
-   def step1_data
-    @lang = LanguagesCollection.all
   end
   def save 
      @user = UserDetail.new()
@@ -62,6 +40,7 @@ class UserController < ApplicationController
      @addskills.skills = session[:value9]
      @language.user_language = session[:value10]
      if @user.save && @quali.save
+      session.clear
       flash[:success] = "your data saved successfully"
       redirect_to user_new_path
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   get 'user/new' , :to => "user#new"
   post 'user/new' , :to => "user#new"
-  post 'user/extra' , :to => "user#extra"
-  get 'user/new_data' , :to => "user#new_data"
-  post 'user/new_data' , :to => "user#step2"
   get 'user/step1', :to => "user#step1"
   post 'user/step1', :to => "user#step1"
-  get 'user/step1_data', :to => "user#step1_data"
-  post 'user/step1_data', :to => "user#step2"
   get 'user/step2', :to => "user#step2"
   post 'user/step2', :to => "user#save"
   
@@ -15,4 +10,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
-	
